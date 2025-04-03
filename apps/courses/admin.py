ea516169f260945be849e1680a8f7329886526c4
Manuchehra <135632769+Manuchexra@users.noreply.github.com>
2025-04-03 09:15:10 +0500

    Courses_admin

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Course, Chapter, Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'level', 'created_by', 'created_at')
+    search_fields = ('title',)
+    list_filter = ('level', 'created_at')
+    ordering = ('-created_at',)
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course')
+    search_fields = ('title', 'course__title')
+    ordering = ('course',)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'chapter', 'is_free_preview')
+    search_fields = ('title', 'chapter__title')
+    list_filter = ('is_free_preview',)
